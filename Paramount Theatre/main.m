96f5dea393acb8fa775a44d3bba3206d1473b511
Mikey Napolitano <moo@awesomegraphics.com>
2011-06-11 00:44:48 -0400

    First Build

--- Paramount Theatre/main.m
+++ Paramount Theatre/main.m
@@ -11,7 +11,11 @@
 int main(int argc, char *argv[])
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    NSArray *data = [NSArray arrayWithContentsOfURL:[NSURL URLWithString:@"http://paramountlive.org/iPhone.php"]];
+    NSLog(@"%@", data);
     int retVal = UIApplicationMain(argc, argv, nil, nil);
+    
     [pool release];
     return retVal;
 }
