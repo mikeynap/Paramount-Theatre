98be665aa49e3659e212079ffdfd808ece07a7e6
Mikey Napolitano <moo@awesomegraphics.com>
2011-06-11 00:33:27 -0400

    Initial Commit

---
+++ Paramount Theatre/Paramount_TheatreAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Paramount_TheatreAppDelegate.h
+//  Paramount Theatre
+//
+//  Created by Moo on 6/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Paramount_TheatreAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ Paramount Theatre/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Paramount Theatre
+//
+//  Created by Moo on 6/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end
